83c3740489cd3e9ec346f2495250b024b1287861
raul <raulhern927@gmail.com>
2014-10-31 12:04:38 -0200

    20141031- Se actualiza mappers.xml

--- cvaucher.services/src/main/java/uy/com/cvaucher/services/mappers/TratamientoMapper.java
+++ cvaucher.services/src/main/java/uy/com/cvaucher/services/mappers/TratamientoMapper.java
@@ -14,17 +14,16 @@ import uy.com.cvaucher.services.domain.Tratamiento;
 
 public interface TratamientoMapper 
 {
-	@Select("SELECT * FROM tratamiento t, tipo_tratamiento tt "
-			+ "WHERE t.trat_tpo_id = tt.tip_trat_id "
-			+ "ORDER BY t.trat_id")
+	@Select("SELECT trat_id, trat_tpo_id, trat_descripcion, trat_cant_sesiones FROM tratamiento "
+			+ "ORDER BY trat_id")
 	@ResultMap("uy.com.cvaucher.services.mappers.TratamientoMapper.TratResult")
 	List<Tratamiento> findAllTratamientos();
 	
-	@Select("SELECT * FROM tratamiento WHERE trat_id=#{tratId}")
+	@Select("SELECT trat_id, trat_tpo_id, trat_descripcion, trat_cant_sesiones FROM tratamiento WHERE trat_id=#{tratId}")
 	@ResultMap("uy.com.cvaucher.services.mappers.TratamientoMapper.TratResult")
 	Tratamiento findTratamientoById(Integer tratId);
 	
-	@Select("SELECT * FROM tratamiento WHERE trat_cedula=#{cedula}")
+	@Select("SELECT trat_id, trat_tpo_id, trat_descripcion, trat_cant_sesiones FROM tratamiento WHERE trat_cedula=#{cedula}")
 	@ResultMap("uy.com.cvaucher.services.mappers.TratamientoMapper.TratResult")
 	List<Tratamiento> findTratamientoByCedula(int cedula);
 	
