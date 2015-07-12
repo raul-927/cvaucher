2165e245f9d03213d343e5b459b9a1d37d55d527
raul <raulhern927@gmail.com>
2015-07-12 12:33:23 -0500

    OK JNI

--- cvaucher.services/src/main/java/uy/com/cvaucher/services/jni/CvaucherJni.java
+++ cvaucher.services/src/main/java/uy/com/cvaucher/services/jni/CvaucherJni.java
@@ -5,7 +5,7 @@ public class CvaucherJni {
 	public native int holaMundo(int a, int b);
 	
 	static{
-		System.load("G:/SERVERS/jboss-as-7.1.1.Final_2/standalone/deployments/cvaucher.services.war/WEB-INF/bin/libcvaucherC.dll");
+		System.loadLibrary("libcvaucherC");
 	}
 
 }
--- cvaucher.services/src/main/java/uy/com/cvaucher/services/services/FormasDePagosService.java
+++ cvaucher.services/src/main/java/uy/com/cvaucher/services/services/FormasDePagosService.java
@@ -140,7 +140,7 @@ public class FormasDePagosService implements FormasDePagosInt
 		int a = 10;
 		int b = 30;
 		int c = cvaucher.holaMundo(a, b);
-		System.out.print("Desde Java, La suma de a + b = "+c);
+		System.out.println("Desde Java, La suma de a + b = "+c);
 		this.tratamientoPacienteMapper.insertTratamientoPacienteMapper(tratamientoPaciente);
 		MaxTratPacId maxTratPacId = new MaxTratPacId();
 		SearchMaxTratPacId search = new SearchMaxTratPacId();
