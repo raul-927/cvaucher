5bf8ad899a19e14cdb2feb374f5567641ad3ab15
Raul Hernandez <raulhern927@gmail.com>
2015-09-09 16:29:42 -0500

    ok

--- cvaucher.services/src/main/java/uy/com/cvaucher/services/enumerador/Prueba.java
+++ cvaucher.services/src/main/java/uy/com/cvaucher/services/enumerador/Prueba.java
@@ -6,17 +6,17 @@ public class Prueba {
 		PruebaEnumerator enumerator = PruebaEnumerator.VALOR_3;
 		
 		for(PruebaEnumerator p: PruebaEnumerator.values()){
-			System.out.println("Los valores son: "+ p);
-			System.out.println("Los Textos 1 son: "+p.getTexto1());
-			System.out.println("Los Numeros 1 son: "+p.getValor1());
-			System.out.println("Los Numeros 2 son: "+p.getValor2());
-			System.out.println("Los Textos 2 son: "+p.getTexto2());
+			System.out.println("Para el "+ p);
+			System.out.println("Texto 1 de "+p.toString()+" es: " +p.getTexto1());
+			System.out.println("Numero 1 de "+p.toString()+" es: "+p.getValor1());
+			System.out.println("Numero 2 de "+p.toString()+" es: "+p.getValor2());
+			System.out.println("Texto 2 de  "+p.toString()+" es: " +p.getTexto2());
 		}
 		
-		System.out.println("El "+enumerator.toString() + " tiene el texto 1: "+enumerator.getTexto1());
+		/*System.out.println("El "+enumerator.toString() + " tiene el texto 1: "+enumerator.getTexto1());
 		System.out.println("El "+enumerator.toString() + " tiene el numero 1: "+enumerator.getValor1());
 		System.out.println("El "+enumerator.toString() + " tiene el numero 2: "+enumerator.getValor2());
-		System.out.println("El "+enumerator.toString() + " tiene el texto 2: "+enumerator.getTexto2());
+		System.out.println("El "+enumerator.toString() + " tiene el texto 2: "+enumerator.getTexto2());*/
 		
 
 	}
