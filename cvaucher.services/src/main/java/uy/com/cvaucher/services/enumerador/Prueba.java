bb1e852293d7d963910522e2f672edbaaf81bc9c
Raul Hernandez <raulhern927@gmail.com>
2015-09-09 17:40:00 -0500

    Ok

--- cvaucher.services/src/main/java/uy/com/cvaucher/services/enumerador/Prueba.java
+++ cvaucher.services/src/main/java/uy/com/cvaucher/services/enumerador/Prueba.java
@@ -7,10 +7,10 @@ public class Prueba {
 		
 		for(PruebaEnumerator p: PruebaEnumerator.values()){
 			System.out.println("Para el "+ p);
-			System.out.println("Texto 1 de "+p.toString()+" es: " +p.getTexto1());
+			System.out.println("Texto 1 de " +p.toString()+" es: "+p.getTexto1());
 			System.out.println("Numero 1 de "+p.toString()+" es: "+p.getValor1());
 			System.out.println("Numero 2 de "+p.toString()+" es: "+p.getValor2());
-			System.out.println("Texto 2 de  "+p.toString()+" es: " +p.getTexto2());
+			System.out.println("Texto 2 de  "+p.toString()+" es: "+p.getTexto2());
 		}
 		
 		/*System.out.println("El "+enumerator.toString() + " tiene el texto 1: "+enumerator.getTexto1());
